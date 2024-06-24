5c8fe7e8d7e0e6036144f7a5528c355c50ed4259
Jon Stovell <jonstovell@gmail.com>
2024-06-24 16:49:30 -0600

    Prevents WYSIWYG autolinking inside Markdown links in editor
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Autolinker.php
+++ Sources/Autolinker.php
@@ -785,12 +785,15 @@ class Autolinker
 		$regexes = self::load()->getJavaScriptUrlRegexes();
 		$regexes['email'] = self::load()->getJavaScriptEmailRegex();
 
+		// Don't autolink if the URL is inside a Markdown link construct.
+		$md_lookbehind = !empty(Config::$modSettings['enableMarkdown']) ? '(?<!\[[^\]]*\](?:\(|:[ \t\u00A0\u1680\u180E\u2000-\u200A\u202F\u205F\u3000]*\n?[ \t\u00A0\u1680\u180E\u2000-\u200A\u202F\u205F\u3000]*))' : '';
+
 		foreach ($regexes as $key => $value) {
-			$js[] = 'autolinker_regexes.set(' . Utils::escapeJavaScript($key) . ', new RegExp(' . Utils::escapeJavaScript($value) . ', "giu"));';
+			$js[] = 'autolinker_regexes.set(' . Utils::escapeJavaScript($key) . ', new RegExp(' . Utils::escapeJavaScript($md_lookbehind . $value) . ', "giu"));';
 
 			$js[] = 'autolinker_regexes.set(' . Utils::escapeJavaScript('paste_' . $key) . ', new RegExp(' . Utils::escapeJavaScript('(?<=^|\s|<br>)' . $value . '(?=$|\s|<br>|\p{Po})') . ', "giu"));';
 
-			$js[] = 'autolinker_regexes.set(' . Utils::escapeJavaScript('keypress_' . $key) . ', new RegExp(' . Utils::escapeJavaScript($value . '(?=[\p{Po}' . preg_quote(implode('', array_merge(array_keys(self::$balanced_pairs), self::$balanced_pairs)), '/') . ']*\s$)') . ', "giu"));';
+			$js[] = 'autolinker_regexes.set(' . Utils::escapeJavaScript('keypress_' . $key) . ', new RegExp(' . Utils::escapeJavaScript($md_lookbehind . $value . '(?=[\p{Po}' . preg_quote(implode('', array_merge(array_keys(self::$balanced_pairs), self::$balanced_pairs)), '/') . ']*\s$)') . ', "giu"));';
 		}
 
 		$js[] = 'const autolinker_balanced_pairs = new Map();';
