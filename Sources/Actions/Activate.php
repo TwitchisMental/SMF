67d59193f86fd50354c0e5c1bb756ea0f08a4bb1
Jon Stovell <jonstovell@gmail.com>
2025-01-08 17:47:19 -0700

    Improves routing for SMF\Actions\Activate
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- Sources/Actions/Activate.php
+++ Sources/Actions/Activate.php
@@ -183,6 +183,48 @@ class Activate implements ActionInterface, Routable
 		];
 	}
 
+	/***********************
+	 * Public static methods
+	 ***********************/
+
+	/**
+	 * Builds a routing path based on URL query parameters.
+	 *
+	 * @param array $params URL query parameters.
+	 * @return array Contains two elements: ['route' => [], 'params' => []].
+	 *    The 'route' element contains the routing path. The 'params' element
+	 *    contains any $params that weren't incorporated into the route.
+	 */
+	public static function buildRoute(array $params): array
+	{
+		$route = self::buildActionRoute($params);
+
+		if (isset($params['u'])) {
+			$route[] = $params['u'];
+			unset($params['u']);
+		}
+
+		return ['route' => $route, 'params' => $params];
+	}
+
+	/**
+	 * Parses a route to get URL query parameters.
+	 *
+	 * @param array $route Array of routing path components.
+	 * @param array $params Any existing URL query parameters.
+	 * @return array URL query parameters
+	 */
+	public static function parseRoute(array $route, array $params = []): array
+	{
+		$params = array_merge($params, self::parseActionRoute($route));
+
+		if (!empty($route)) {
+			$params['u'] = array_shift($route);
+		}
+
+		return $params;
+	}
+
 	/******************
 	 * Internal methods
 	 ******************/
